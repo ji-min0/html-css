afddfb8d34c67e8d724ec8a13c2aa59f563d1b80
ji-min0 <k.jimin2002@gmail.com>
2025-09-15 14:58:09 +0900

    feat: Mini Project : 회원가입 폼 만들기 추가

---
+++ 3회차/join.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault() // 폼 제출 기본 동작 방지
+
+    let userId = event.target.elements["id"].value
+    let userPw = event.target.elements["password"].value
+    let userPwCheck = event.target.elements["passwordCheck"].value
+    let userName = event.target.elements["name"].value
+    let userPhone = event.target.elements["phone"].value
+    let userPosition = event.target.elements["position"].value
+    let userGender = event.target.elements["gender"].value
+    let userEmail = event.target.elements["email"].value
+    let userIntro = event.target.elements["intro"].value
+
+    /*아이디 길이 조정*/
+    if (userId.length < 5 || userId.length > 20) {
+        alert("아이디는 5자 이상 20자 이하로 입력해주세요.")
+        return
+    }
+
+    /*비밀번호 확인*/
+    if (userPw !== userPwCheck) {
+        alert("비밀번호가 일치하지 않습니다.")
+        return
+    }
+
+    /*가입 환영 메시지*/
+    document.body.innerHTML = `<h1>${userName}님 환영합니다!</h1>`
+})
